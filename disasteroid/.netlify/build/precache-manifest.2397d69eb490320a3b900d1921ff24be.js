d937f973bafcba8a01e752267af11cc91971cba1
Nahida Islam <nahida@kth.se>
2020-01-01 22:33:21 +0100

    deployment

---
+++ disasteroid/.netlify/build/precache-manifest.2397d69eb490320a3b900d1921ff24be.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8131deb15c962db0371264628eb33e08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e220cc2f6d84ab76e22b",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "fc263a523b3cad552dce",
+    "url": "/static/css/main.71562722.chunk.css"
+  },
+  {
+    "revision": "e220cc2f6d84ab76e22b",
+    "url": "/static/js/2.dbd317bc.chunk.js"
+  },
+  {
+    "revision": "fc263a523b3cad552dce",
+    "url": "/static/js/main.98a6d0a0.chunk.js"
+  },
+  {
+    "revision": "1d0e3fd0aad0c29ea986",
+    "url": "/static/js/runtime-main.16d05c55.js"
+  },
+  {
+    "revision": "b214afaec3311ffd34835cd335af09c2",
+    "url": "/static/media/ARCore.b214afae.jpg"
+  },
+  {
+    "revision": "7ae9d306c9b88e69fa6b17cd8e8e2628",
+    "url": "/static/media/arturs.7ae9d306.JPG"
+  },
+  {
+    "revision": "624cf337c8ac299d8817b2bbc2a2b236",
+    "url": "/static/media/augustin.624cf337.JPG"
+  },
+  {
+    "revision": "03ffca35b0ba1deed5e18ef79ae060be",
+    "url": "/static/media/blender.03ffca35.jpg"
+  },
+  {
+    "revision": "2aa9cc143e65dded30847b10a80fad25",
+    "url": "/static/media/chantal.2aa9cc14.JPG"
+  },
+  {
+    "revision": "0097a6765f0d07a7ec406ec38d2b8de4",
+    "url": "/static/media/earth.0097a676.png"
+  },
+  {
+    "revision": "009b75265419fc80d302f1cb0032bf70",
+    "url": "/static/media/image1.009b7526.jpg"
+  },
+  {
+    "revision": "72f91517f8e3a5f1c2b640cff0d6533d",
+    "url": "/static/media/image2.72f91517.jpg"
+  },
+  {
+    "revision": "b20e65d3403b7e9391ab8d4fb1c6d6f8",
+    "url": "/static/media/image3.b20e65d3.jpg"
+  },
+  {
+    "revision": "b7d6e3c693ec71ce1147e8dbbc8b6553",
+    "url": "/static/media/image4.b7d6e3c6.jpg"
+  },
+  {
+    "revision": "55c4f83900eebd6b198286b158239e70",
+    "url": "/static/media/image5.55c4f839.jpg"
+  },
+  {
+    "revision": "da1c64544f041bd6d0e715010b03b0e3",
+    "url": "/static/media/image6.da1c6454.JPG"
+  },
+  {
+    "revision": "0b57d76f7e77313e1dbcd6c1927760bd",
+    "url": "/static/media/image7.0b57d76f.JPG"
+  },
+  {
+    "revision": "c3af70aa2a6ce7d170314f9a6baa8e17",
+    "url": "/static/media/image8.c3af70aa.JPG"
+  },
+  {
+    "revision": "2e23d59fc480f5d48340d169cd501fcb",
+    "url": "/static/media/miki.2e23d59f.jpg"
+  },
+  {
+    "revision": "e8ff9dbf1553673888f545a9ff7fb333",
+    "url": "/static/media/nahida.e8ff9dbf.jpg"
+  },
+  {
+    "revision": "1165b5a51c0b3334ef968819d48ee68c",
+    "url": "/static/media/tingyi.1165b5a5.JPG"
+  },
+  {
+    "revision": "437ebf4d96701f9c146bf24123f77753",
+    "url": "/static/media/unity.437ebf4d.png"
+  }
+]);
